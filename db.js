eea67acf87542b4a0fe4da36c78833ccb69da580
Mertcan Segmen <mertcan.segmen@hotmail.com>
2023-09-25 01:23:38 +0300

    added db functions for target user tags

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ const DB_VERSION = "1";
 
 const KEY_DB_VERSION = "KEY_DB_VERSION";
 const KEY_COMMAND_CHANNELS = "KEY_COMMAND_CHANNELS";
+const KEY_TARGET_USER_TAGS = "KEY_TARGET_USER_TAGS";
 
 const updateDbIfNewerVersion = async () => {
   const existingDbVersion = await db.get(KEY_DB_VERSION);
@@ -82,10 +83,111 @@ const clearCommandChannel = async (guildId) => {
   await db.set(KEY_COMMAND_CHANNELS, updatedCommandChannels);
 };
 
+/**
+ * Gets all target user tags for guilds. If there are no entries, it returns an empty array.
+ * @return {Array<Object>} - All target user tags for guilds
+ */
+const getAllTargetUserTags = async () => {
+  const targetUserTags = await db.get(KEY_TARGET_USER_TAGS);
+  return targetUserTags || [];
+};
+
+/**
+ * Returns the target user tag list for a guild.
+ * @param {string} guildId - Id of the guild
+ * @returns {Array<string>} - List of target user tags
+ */
+const getTargetUserTags = async (guildId) => {
+  const allTargetUserTags = await getAllTargetUserTags();
+  const guildEntry = allTargetUserTags.find((entry) => entry.guildId === guildId);
+  return guildEntry ? guildEntry.targetUserTags : [];
+};
+
+/**
+ * Adds a target user tag to a guild.
+ * Throws an error if the target user tag already exists for the guild.
+ * @param {string} guildId - Id of the guild
+ * @param {string} targetUserTag - User tag to add
+ */
+const addTargetUserTag = async (guildId, targetUserTag) => {
+  const allTargetUserTags = await getAllTargetUserTags();
+  const guildEntryIndex = allTargetUserTags.findIndex(
+    (entry) => entry.guildId === guildId
+  );
+
+  if (guildEntryIndex !== -1) {
+    const targetUserTags = allTargetUserTags[guildEntryIndex].targetUserTags;
+    if (targetUserTags.includes(targetUserTag)) {
+      throw new Error(`Target user tag "${targetUserTag}" already exists for the guild.`);
+    } else {
+      // Target user tag does not exist, add it
+      targetUserTags.push(targetUserTag);
+    }
+  } else {
+    // Guild entry doesn't exist, create a new one with the target user tag
+    allTargetUserTags.push({
+      guildId,
+      targetUserTags: [targetUserTag],
+    });
+  }
+
+  await db.set(KEY_TARGET_USER_TAGS, allTargetUserTags);
+};
+
+/**
+ * Removes a target user tag from a guild.
+ * @param {string} guildId - Id of the guild
+ * @param {string} targetUserTag - User tag to remove
+ */
+const removeTargetUserTag = async (guildId, targetUserTag) => {
+  const allTargetUserTags = await getAllTargetUserTags();
+  const guildEntryIndex = allTargetUserTags.findIndex(
+    (entry) => entry.guildId === guildId
+  );
+
+  if (guildEntryIndex !== -1) {
+    const targetUserTags = allTargetUserTags[guildEntryIndex].targetUserTags;
+    const tagIndex = targetUserTags.indexOf(targetUserTag);
+
+    if (tagIndex !== -1) {
+      // User tag found, remove it
+      targetUserTags.splice(tagIndex, 1);
+
+      // If there are no more tags, remove the guild entry
+      if (targetUserTags.length === 0) {
+        allTargetUserTags.splice(guildEntryIndex, 1);
+      }
+
+      await db.set(KEY_TARGET_USER_TAGS, allTargetUserTags);
+    } else {
+      throw new Error(`Target user tag "${targetUserTag}" not found.`);
+    }
+  } else {
+    throw new Error(`No target user exists for removal.`);
+  }
+};
+
+/**
+ * Removes all target user tags for a guild.
+ * @param {string} guildId - Id of the guild
+ */
+const clearTargetUserTags = async (guildId) => {
+  const allTargetUserTags = await getAllTargetUserTags();
+  const filteredTargetUserTags = allTargetUserTags.filter(
+    (entry) => entry.guildId !== guildId
+  );
+
+  await db.set(KEY_TARGET_USER_TAGS, filteredTargetUserTags);
+};
 
 module.exports = {
   updateDbIfNewerVersion,
   getCommandChannel,
   setCommandChannel,
   clearCommandChannel,
+  getAllTargetUserTags,
+  getTargetUserTags,
+  addTargetUserTag,
+  removeTargetUserTag,
+  clearTargetUserTags,
 };
