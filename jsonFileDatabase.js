7f19e711b8dd2177a77d34b8f89dfa77e9a8225b
Mertcan Segmen <mertcan.segmen@hotmail.com>
2023-09-26 00:32:54 +0300

    added documentation to the json file db functions

--- jsonFileDatabase.js
+++ jsonFileDatabase.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * A simple JSON file-based database.
+ */
 class JSONFileDatabase {
+  /**
+   * Creates a new instance of the JSONFileDatabase.
+   * @param {string} folderPath - The path to the folder where JSON files will be stored.
+   */
   constructor(folderPath) {
     this.folderPath = folderPath;
   }
 
+  /**
+   * Gets the value associated with a key from the database.
+   * @param {string} key - The key to retrieve data for.
+   * @returns {Promise<any|null>} - The value associated with the key, or null if not found.
+   */
   async get(key) {
     const filePath = this.getFilePath(key);
     if (!fs.existsSync(filePath)) {
@@ -16,12 +28,21 @@ class JSONFileDatabase {
     return JSON.parse(rawData);
   }
 
+  /**
+   * Sets the value associated with a key in the database.
+   * @param {string} key - The key to set data for.
+   * @param {any} value - The value to set.
+   */
   async set(key, value) {
     const filePath = this.getFilePath(key);
     const jsonData = JSON.stringify(value, null, 0);
     fs.writeFileSync(filePath, jsonData, 'utf8');
   }
 
+  /**
+   * Deletes the value associated with a key from the database.
+   * @param {string} key - The key to delete data for.
+   */
   async delete(key) {
     const filePath = this.getFilePath(key);
     if (fs.existsSync(filePath)) {
@@ -29,6 +50,9 @@ class JSONFileDatabase {
     }
   }
 
+  /**
+   * Empties the entire database by deleting all files.
+   */
   async empty() {
     fs.readdirSync(this.folderPath).forEach((file) => {
       const filePath = path.join(this.folderPath, file);
@@ -36,6 +60,11 @@ class JSONFileDatabase {
     });
   }
 
+  /**
+   * Gets the file path for a given key.
+   * @param {string} key - The key for which to generate the file path.
+   * @returns {string} - The file path.
+   */
   getFilePath(key) {
     return path.join(this.folderPath, `${key}.json`);
   }
