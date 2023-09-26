663d732c8362f39a8e5937e68eefb21a1b2f2906
Mertcan Segmen <mertcan.segmen@hotmail.com>
2023-09-26 20:48:48 +0300

    moved the command execution logic to another script called commandHandler.js

---
+++ commandHandler.js
@@ -0,0 +1,88 @@
+const db = require('./db');
+
+const ping = async interaction => {
+    interaction.reply("I'm alive!")
+};
+
+const commandChannelShow = async interaction => {
+    const guildId = interaction.guild.id;
+
+    const commandChannel = await db.getCommandChannel(guildId);
+
+    let reply = "";
+    if (!commandChannel) {
+      reply = "No command channel was set. Use command-channel-set command for setting a command channel."
+    } else {
+      reply = "Command channel: " + commandChannel;
+    }
+
+    interaction.reply(reply);
+}
+
+const commandChannelSet = async interaction => {
+    const guildId = interaction.guild.id;
+    const commandChannelName = interaction.options.get("command-channel").value;
+
+    await db.setCommandChannel(guildId, commandChannelName);
+
+    interaction.reply("Set " + commandChannelName + " as the command channel.");
+}
+
+const commandChannelClear = async interaction => {
+    const guildId = interaction.guild.id;
+
+    await db.clearCommandChannel(guildId);
+
+    interaction.reply("Cleared the set command channel.");
+}
+
+const userTagsList = async interaction => {
+    const guildId = interaction.guild.id;
+
+    const userTags = await db.getTargetUserTags(guildId);
+
+    let reply = "";
+    if (!userTags.length) {
+      reply = "No target user tag found. Use user-tags-add command for adding new target user tags."
+    } else {
+      reply = "Target user tags: " + userTags.join(", ") + ".";
+    }
+
+    interaction.reply(reply);
+}
+
+const userTagsAdd = async interaction => {
+    const userTag = interaction.options.get("user-tag").value;
+    const guildId = interaction.guild.id;
+
+    try {
+      await db.addTargetUserTag(guildId, userTag);
+
+      interaction.reply("Added " + userTag + " to the target list.");
+    } catch (err) {
+      interaction.reply(err.message)
+    }
+}
+
+const userTagsRemove = async interaction => {
+    const userTag = interaction.options.get("user-tag").value;
+    const guildId = interaction.guild.id;
+
+    try {
+      await db.removeTargetUserTag(guildId, userTag);
+
+      interaction.reply("Removed " + userTag + " from the target list.");
+    } catch (err) {
+      interaction.reply(err.message)
+    }
+}
+
+module.exports = {
+    ping,
+    commandChannelShow,
+    commandChannelSet,
+    commandChannelClear,
+    userTagsList,
+    userTagsAdd,
+    userTagsRemove,
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const utils = require("./utils.js");
 const db = require("./db.js");
 const commandRegistration = require("./commandRegistration");
+const commandHandler = require("./commandHandler");
 
 const CLEAN_UP_DELAY_IN_MS = 20 * 1000;
 
@@ -33,70 +34,32 @@ client.on('interactionCreate', async interaction => {
     return;
   }
 
-  if (interaction.commandName === "ping") {
-    interaction.reply("I'm alive!")
-  } else if (interaction.commandName === "command-channel-show") {
-    const guildId = interaction.guild.id;
-
-    const commandChannel = await db.getCommandChannel(guildId);
-
-    let reply = "";
-    if (!commandChannel) {
-      reply = "No command channel was set. Use command-channel-set command for setting a command channel."
-    } else {
-      reply = "Command channel: " + commandChannel;
-    }
-
-    interaction.reply(reply);
-  } else if (interaction.commandName === "command-channel-set") {
-    const guildId = interaction.guild.id;
-    const commandChannelName = interaction.options.get("command-channel").value;
-
-    await db.setCommandChannel(guildId, commandChannelName);
-
-    interaction.reply("Set " + commandChannelName + " as the command channel.");
-  } else if (interaction.commandName === "command-channel-clear") {
-    const guildId = interaction.guild.id;
-
-    await db.clearCommandChannel(guildId);
-
-    interaction.reply("Cleared the set command channel.");
-  } else if (interaction.commandName === "user-tags-list") {
-    const guildId = interaction.guild.id;
-
-    const userTags = await db.getTargetUserTags(guildId);
-
-    let reply = "";
-    if (!userTags.length) {
-      reply = "No target user tag found. Use user-tags-add command for adding new target user tags."
-    } else {
-      reply = "Target user tags: " + userTags.join(", ") + ".";
-    }
-
-    interaction.reply(reply);
-  } else if (interaction.commandName === "user-tags-add") {
-    const userTag = interaction.options.get("user-tag").value;
-    const guildId = interaction.guild.id;
-
-    try {
-      await db.addTargetUserTag(guildId, userTag);
-
-      interaction.reply("Added " + userTag + " to the target list.");
-    } catch (err) {
-      interaction.reply(err.message)
-    }
-  } else if (interaction.commandName === "user-tags-remove") {
-    const userTag = interaction.options.get("user-tag").value;
-    const guildId = interaction.guild.id;
-
-    try {
-      await db.removeTargetUserTag(guildId, userTag);
-
-      interaction.reply("Removed " + userTag + " from the target list.");
-    } catch (err) {
-      interaction.reply(err.message)
-    }
+  switch (interaction.commandName) {
+    case "ping":
+      commandHandler.ping(interaction);
+      break;
+    case "command-channel-show":
+      commandHandler.commandChannelShow(interaction);
+      break;
+    case "command-channel-set":
+      commandHandler.commandChannelSet(interaction);
+      break;
+    case "command-channel-clear":
+      commandHandler.commandChannelClear(interaction);
+      break;
+    case "user-tags-list":
+      commandHandler.userTagsList(interaction);
+      break;
+    case "user-tags-add":
+      commandHandler.userTagsAdd(interaction);
+      break;
+    case "user-tags-remove":
+      commandHandler.userTagsRemove(interaction);
+      break;
+    default:
+      break;
   }
+
 })
 
 client.on('messageCreate', async message => {
